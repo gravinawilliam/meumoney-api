a0c4057873175a86955cb92b70ab08f83eeafc4a
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 08:14:18 -0300

    feat: interface created for the user repository

---
+++ src/modules/users/interfaces/repositories/IUsersRepository.ts
@@ -0,0 +1,6 @@
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import IUser from '../models/IUser';
+
+export default interface IUsersRepository {
+  create(user: ICreateUserDTO): Promise<IUser>;
+}
