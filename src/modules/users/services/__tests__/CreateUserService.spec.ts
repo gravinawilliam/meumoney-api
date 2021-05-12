0d6f5f58329cd0b03344979ae303d341a9ffef22
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 09:04:50 -0300

    test: added hash password in create user

--- src/modules/users/services/__tests__/CreateUserService.spec.ts
+++ src/modules/users/services/__tests__/CreateUserService.spec.ts
@@ -1,13 +1,16 @@
 import FakeUsersRepository from '@modules/users/fakes/FakeUsersRepository';
+import FakeHashProvider from '@shared/container/providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from '../CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
 let createUser: CreateUserService;
 
 describe('CreateUser', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
-    createUser = new CreateUserService(fakeUsersRepository);
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
   });
 
   it('should be able to create user', async () => {
@@ -17,5 +20,15 @@ describe('CreateUser', () => {
       name: 'William',
     });
     expect(user).toHaveProperty('id');
+  });
+
+  it('must be able to hash the password', async () => {
+    const user = await createUser.execute({
+      email: 'william@example.com',
+      password: '123456789',
+      name: 'William',
+    });
+    const hashedPassword = await fakeHashProvider.generateHash('123456789');
+    expect(user.password).toBe(hashedPassword);
   });
 });
