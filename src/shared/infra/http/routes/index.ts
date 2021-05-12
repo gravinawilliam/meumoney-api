c7e58f5ec6a4ebc4579d5e2ababa50951aa01bb2
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 14:30:04 -0300

    feat: added banks route in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import users from '@modules/users/infra/http/routes/users.routes';
 import sessions from '@modules/users/infra/http/routes/sessions.routes';
+import banks from '@modules/banks/infra/http/routes/banks.routes';
 
 const routes = Router();
 
 routes.use('/users', users);
 routes.use('/sessions', sessions);
+routes.use('/banks', banks);
 
 export default routes;
