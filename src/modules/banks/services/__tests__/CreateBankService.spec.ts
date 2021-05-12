9e1aab022606b27a31f33a976a137746f7dd81ea
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 14:46:04 -0300

    test: should not be able to create a new bank with an existing name

--- src/modules/banks/services/__tests__/CreateBankService.spec.ts
+++ src/modules/banks/services/__tests__/CreateBankService.spec.ts
@@ -1,4 +1,5 @@
 import FakeBanksRepository from '@modules/banks/fakes/FakeBanksRepository';
+import AppError from '@shared/errors/AppError';
 import CreateBankService from '../CreateBankService';
 
 let fakeBanksRepository: FakeBanksRepository;
@@ -18,5 +19,22 @@ describe('AuthenticateUser', () => {
       redColorCard: 31,
     });
     expect(bank).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new bank with an existing name', async () => {
+    await fakeBanksRepository.create({
+      name: 'Nubank',
+      blueColorCard: 211,
+      greenColorCard: 123,
+      redColorCard: 31,
+    });
+    await expect(
+      createBank.execute({
+        name: 'Nubank',
+        blueColorCard: 211,
+        greenColorCard: 123,
+        redColorCard: 31,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
