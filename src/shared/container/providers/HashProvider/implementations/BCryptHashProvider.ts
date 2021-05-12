aa2a72b5205f7819003e27dd7bc35dfd82d25394
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 10:17:41 -0300

    feat: added compareHash method in bcrypt hash provider

--- src/shared/container/providers/HashProvider/implementations/BCryptHashProvider.ts
+++ src/shared/container/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -1,8 +1,12 @@
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import IHashProvider from '../interfaces/IHashProvider';
 
 export default class BCryptHashProvider implements IHashProvider {
   public async generateHash(payload: string): Promise<string> {
     return hash(payload, 10);
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return compare(payload, hashed);
   }
 }
