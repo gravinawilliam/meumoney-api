3bfc083eb76c89b8c7dfe3807ecf2d46ca0ad232
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 10:15:45 -0300

    feat: added compareHash method

--- src/shared/container/providers/HashProvider/fakes/FakeHashProvider.ts
+++ src/shared/container/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -4,4 +4,8 @@ export default class FakeHashProvider implements IHashProvider {
   public async generateHash(payload: string): Promise<string> {
     return payload;
   }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return payload === hashed;
+  }
 }
