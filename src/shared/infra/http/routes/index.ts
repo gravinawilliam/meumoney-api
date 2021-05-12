172930ef422ce9b52455ec5a33439a8ef76aee5e
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 08:52:42 -0300

    feat: added user route in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import users from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
+
+routes.use('/users', users);
 
 export default routes;
