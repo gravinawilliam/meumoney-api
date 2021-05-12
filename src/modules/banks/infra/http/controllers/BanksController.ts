32c2362497a249f5d8e22ec48a0c05b3e0de83e7
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 16:19:01 -0300

    feat: added index method for list banks

--- src/modules/banks/infra/http/controllers/BanksController.ts
+++ src/modules/banks/infra/http/controllers/BanksController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 import CreateBankService from '@modules/banks/services/CreateBankService';
+import ListBanksService from '@modules/banks/services/ListBanksService';
 
 export default class BanksController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -14,5 +15,11 @@ export default class BanksController {
       redColorCard,
     });
     return res.status(201).json(classToClass(bank));
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listBanks = container.resolve(ListBanksService);
+    const banks = await listBanks.execute();
+    return res.status(201).json(classToClass(banks));
   }
 }
