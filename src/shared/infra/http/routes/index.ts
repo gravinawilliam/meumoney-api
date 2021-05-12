efadcd7f4f7d250e61a05ea70f45b5816e062d88
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 11:00:24 -0300

    feat: added sessions route in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import users from '@modules/users/infra/http/routes/users.routes';
+import sessions from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', users);
+routes.use('/sessions', sessions);
 
 export default routes;
