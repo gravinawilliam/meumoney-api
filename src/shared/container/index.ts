67262683e0deeb56b48bf87bb1b94c8cd71ce597
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-12 14:17:22 -0300

    chore: added injection for users roles repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
+import UserRolesRepository from '@modules/users/infra/typeorm/repositories/UserRolesRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import IUserRolesRepository from '@modules/users/interfaces/repositories/IUserRolesRepository';
 import IUsersRepository from '@modules/users/interfaces/repositories/IUsersRepository';
 import { container } from 'tsyringe';
 import './providers';
@@ -6,4 +8,9 @@ import './providers';
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserRolesRepository>(
+  'UserRolesRepository',
+  UserRolesRepository,
 );
