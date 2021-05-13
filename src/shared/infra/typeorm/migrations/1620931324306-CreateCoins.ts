43da41c4cc8f89bc6cf54c98769818b2bbe62047
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-13 15:58:59 -0300

    feat: created migration for coins

---
+++ src/shared/infra/typeorm/migrations/1620931324306-CreateCoins.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCoins1620931324306 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'coins',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'symbol',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('coins');
+  }
+}
