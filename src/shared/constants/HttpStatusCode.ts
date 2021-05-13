c7a79f2adad6194d4a9221a2c2679c7afe46f01b
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-13 14:17:13 -0300

    feat: created constants for http status code

---
+++ src/shared/constants/HttpStatusCode.ts
@@ -0,0 +1,8 @@
+export const OK = 200;
+export const CREATED = 201;
+export const ACCEPTED = 202;
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const NOT_FOUND = 404;
+export const NOT_ACCEPTABLE = 406;
+export const CONFLICT = 409;
