6f6d0c8417e4295458eadfa450092cc2eeea4653
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-15 15:33:52 -0300

    feat: added method findNineCoins in interface coins repository

--- src/modules/coins/interfaces/repositories/ICoinsRepository.ts
+++ src/modules/coins/interfaces/repositories/ICoinsRepository.ts
@@ -3,6 +3,7 @@ import ICoin from '../models/ICoin';
 
 export default interface ICoinsRepository {
   create(coin: ICreateCoinDTO): Promise<ICoin>;
+  findNineCoins(): Promise<ICoin[]>;
   findByName(name: string): Promise<ICoin | undefined>;
   findBySymbol(symbol: string): Promise<ICoin | undefined>;
   save(coin: ICoin): Promise<ICoin>;
