877f056339bdf3ba5992923187907dad806d73af
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-15 15:47:37 -0300

    feat: method created for list coins

--- src/modules/coins/infra/http/controllers/CoinsController.ts
+++ src/modules/coins/infra/http/controllers/CoinsController.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
-import { CREATED } from '@shared/constants/HttpStatusCode';
+import { CREATED, OK } from '@shared/constants/HttpStatusCode';
 import CreateCoinService from '@modules/coins/services/CreateCoinService';
+import ListCoinsService from '@modules/coins/services/ListCoinsService';
 
 export default class CoinsController {
   public async create(req: Request, res: Response): Promise<Response> {
-    const createBank = container.resolve(CreateCoinService);
-    const httpResponse = await createBank.execute();
+    const createCoin = container.resolve(CreateCoinService);
+    const httpResponse = await createCoin.execute();
     return res.status(CREATED).json(classToClass(httpResponse));
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const listCoins = container.resolve(ListCoinsService);
+    const httpResponse = await listCoins.execute();
+    return res.status(OK).json(classToClass(httpResponse));
   }
 }
