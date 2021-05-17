ee100e5606c772743f51e5d2cc3f931cc0c9db26
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-17 12:02:32 -0300

    feat: added method findById in interface banks repository

--- src/modules/banks/interfaces/repositories/IBanksRepository.ts
+++ src/modules/banks/interfaces/repositories/IBanksRepository.ts
@@ -4,5 +4,6 @@ import IBank from '../models/IBank';
 export default interface IBanksRepository {
   create(bank: ICreateBankDTO): Promise<IBank>;
   findAll(): Promise<IBank[]>;
+  findById(id: string): Promise<IBank | undefined>;
   findByName(name: string): Promise<IBank | undefined>;
 }
