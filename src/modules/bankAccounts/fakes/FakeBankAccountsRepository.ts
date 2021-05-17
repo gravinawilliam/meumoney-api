2798ea1d29f49946de6565bbdcf98c1ae369ce65
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-17 00:52:35 -0300

    feat: fake created for bank accounts repository

---
+++ src/modules/bankAccounts/fakes/FakeBankAccountsRepository.ts
@@ -0,0 +1,22 @@
+import { v4 } from 'uuid';
+import BankAccount from '../infra/typeorm/entities/BankAccount';
+import ICreateBankAccountDTO from '../interfaces/dtos/ICreateBankAccountDTO';
+import IBankAccount from '../interfaces/models/IBankAccount';
+import IBankAccountsRepository from '../interfaces/repositories/IBankAccountsRepository';
+
+export default class FakeBankAccountsRepository
+  implements IBankAccountsRepository
+{
+  private bankAccounts: IBankAccount[] = [];
+
+  public async create(
+    bankAccount: ICreateBankAccountDTO,
+  ): Promise<IBankAccount> {
+    const bankCreated = Object.assign(new BankAccount(), {
+      id: v4(),
+      ...bankAccount,
+    });
+    this.bankAccounts.push(bankCreated);
+    return bankCreated;
+  }
+}
