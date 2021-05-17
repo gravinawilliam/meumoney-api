aae53b7055eef5221c675dca024ddc135d8417d0
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-17 00:54:43 -0300

    feat: created enum symbol coin

---
+++ src/modules/bankAccounts/interfaces/enums/SymbolCoinEnum.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-shadow
+enum SymbolCoinEnum {
+  'GBP',
+  'ARS',
+  'CAD',
+  'AUD',
+  'JPY',
+  'CNY',
+  'BTC',
+  'EUR',
+  'USD',
+}
+
+export default SymbolCoinEnum;
