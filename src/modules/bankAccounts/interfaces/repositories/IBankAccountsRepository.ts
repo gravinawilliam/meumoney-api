693ff02694083c1d44d0f38e9c7323fa82f110ee
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-17 00:53:16 -0300

    feat: interface created for bank account repository

---
+++ src/modules/bankAccounts/interfaces/repositories/IBankAccountsRepository.ts
@@ -0,0 +1,6 @@
+import ICreateBankAccountDTO from '../dtos/ICreateBankAccountDTO';
+import IBankAccount from '../models/IBankAccount';
+
+export default interface IBankAccountsRepository {
+  create(bankAccount: ICreateBankAccountDTO): Promise<IBankAccount>;
+}
