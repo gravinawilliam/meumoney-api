de9b2c943554277094a60d5dfd62e6283a14164e
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-17 01:01:37 -0300

    feat: added route bank accounts in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import users from '@modules/users/infra/http/routes/users.routes';
 import sessions from '@modules/users/infra/http/routes/sessions.routes';
 import banks from '@modules/banks/infra/http/routes/banks.routes';
 import coins from '@modules/coins/infra/http/routes/coins.routes';
+import bankAccounts from '@modules/bankAccounts/infra/http/routes/bankAccounts.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/users', users);
 routes.use('/sessions', sessions);
 routes.use('/banks', banks);
 routes.use('/coins', coins);
+routes.use('/bankAccounts', bankAccounts);
 
 export default routes;
