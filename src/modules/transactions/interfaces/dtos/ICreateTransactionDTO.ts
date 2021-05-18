d7fafacb79ec22277606db87a6c9d8daa99d03b0
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-18 00:36:00 -0300

    feat: dto created for create transaction

---
+++ src/modules/transactions/interfaces/dtos/ICreateTransactionDTO.ts
@@ -0,0 +1,11 @@
+export default interface ICreateTransactionDTO {
+  value: number;
+  transactionType: string;
+  title: string;
+  note: string;
+  date: Date;
+  symbolCoin: string;
+  userId: string;
+  fromBankAccountId: string;
+  toBankAccountId: string;
+}
