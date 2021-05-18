4bdf9f2588478d179742a4b707c72c843330912b
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-18 00:57:50 -0300

    feat: added route profiles in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import users from '@modules/users/infra/http/routes/users.routes';
 import sessions from '@modules/users/infra/http/routes/sessions.routes';
+import profiles from '@modules/users/infra/http/routes/profiles.routes';
 import banks from '@modules/banks/infra/http/routes/banks.routes';
 import coins from '@modules/coins/infra/http/routes/coins.routes';
 import bankAccounts from '@modules/bankAccounts/infra/http/routes/bankAccounts.routes';
@@ -14,5 +15,6 @@ routes.use('/banks', banks);
 routes.use('/coins', coins);
 routes.use('/bankAccounts', bankAccounts);
 routes.use('/transactions', transactions);
+routes.use('/profiles', profiles);
 
 export default routes;
