3145b135195e6eb337e9a1962653bd9690bc0680
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-18 00:28:28 -0300

    feat: methods created for find by id and save in interface bank accounts repository

--- src/modules/bankAccounts/interfaces/repositories/IBankAccountsRepository.ts
+++ src/modules/bankAccounts/interfaces/repositories/IBankAccountsRepository.ts
@@ -4,4 +4,6 @@ import IBankAccount from '../models/IBankAccount';
 export default interface IBankAccountsRepository {
   create(bankAccount: ICreateBankAccountDTO): Promise<IBankAccount>;
   findByUserId(userId: string): Promise<IBankAccount[]>;
+  findById(id: string): Promise<IBankAccount | undefined>;
+  save(bankAccount: IBankAccount): Promise<IBankAccount>;
 }
