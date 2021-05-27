b6468eb22bed5e33a962a965b8d8ca99e94804f6
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-27 20:09:20 -0300

    feat: route created for list transaction by date and user id

--- src/modules/transactions/infra/http/routes/transactions.routes.ts
+++ src/modules/transactions/infra/http/routes/transactions.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import TransactionsController from '../controllers/TransactionsController';
+import UserTransactionsByDateController from '../controllers/UserTransactionsByDateController';
+import listTransactionsByDateUserIdValidator from '../validators/ListTransactionsByDateUserId';
 
 const transactionsRouter = Router();
 const transactionsController = new TransactionsController();
+const userTransactionsByDateController = new UserTransactionsByDateController();
 
 transactionsRouter.use(ensureAuthenticated);
 
 transactionsRouter.post('/', transactionsController.create);
+transactionsRouter.get(
+  '/',
+  listTransactionsByDateUserIdValidator,
+  userTransactionsByDateController.index,
+);
 
 export default transactionsRouter;
