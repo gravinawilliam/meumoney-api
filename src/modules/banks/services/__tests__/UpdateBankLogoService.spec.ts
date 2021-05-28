4057207eb122217dac9249783ba7515c49639a69
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-28 08:55:21 -0300

    test: should be able to update bank logo

---
+++ src/modules/banks/services/__tests__/UpdateBankLogoService.spec.ts
@@ -0,0 +1,32 @@
+import FakeBanksRepository from '@modules/banks/fakes/FakeBanksRepository';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import UpdateBankLogoService from '../UpdateBankLogoService';
+
+let fakeBanksRepository: FakeBanksRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateBankLogo: UpdateBankLogoService;
+
+describe('Update Bank Logo', () => {
+  beforeEach(() => {
+    fakeBanksRepository = new FakeBanksRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+    updateBankLogo = new UpdateBankLogoService(
+      fakeBanksRepository,
+      fakeStorageProvider,
+    );
+  });
+
+  it('should be able to update bank logo', async () => {
+    const bank = await fakeBanksRepository.create({
+      name: 'Nubank',
+      blueColorCard: 211,
+      greenColorCard: 123,
+      redColorCard: 31,
+    });
+    const updatedBankLogo = await updateBankLogo.execute({
+      bankId: bank.id,
+      logo: 'logo.jpg',
+    });
+    expect(updatedBankLogo.logo).toBe('logo.jpg');
+  });
+});
