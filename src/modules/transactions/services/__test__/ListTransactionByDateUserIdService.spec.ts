6ee2faa1e27c186e565862b555e1e9d94e3779cf
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-29 00:38:44 -0300

    test: should be able to list transactions by date and user id

---
+++ src/modules/transactions/services/__test__/ListTransactionByDateUserIdService.spec.ts
@@ -0,0 +1,64 @@
+import FakeBanksRepository from '@modules/banks/fakes/FakeBanksRepository';
+import FakeTransactionsRepository from '@modules/transactions/fakes/FakeTransactionsRepository';
+import FakeUsersRepository from '@modules/users/fakes/FakeUsersRepository';
+import ListTransactionByDateUserIdService from '../ListTransactionByDateUserIdService';
+
+let fakeTransactionsRepository: FakeTransactionsRepository;
+let fakeUsersRepository: FakeUsersRepository;
+let fakeBanksRepository: FakeBanksRepository;
+let listTransactionByDateUserId: ListTransactionByDateUserIdService;
+
+describe('List Transaction By Date User Id', () => {
+  beforeEach(() => {
+    fakeTransactionsRepository = new FakeTransactionsRepository();
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeBanksRepository = new FakeBanksRepository();
+    listTransactionByDateUserId = new ListTransactionByDateUserIdService(
+      fakeTransactionsRepository,
+      fakeUsersRepository,
+    );
+  });
+
+  it('should be able to list transactions by date and user id', async () => {
+    const bank = await fakeBanksRepository.create({
+      blueColorCard: 123,
+      greenColorCard: 154,
+      redColorCard: 133,
+      name: 'Nubank',
+    });
+
+    const user = await fakeUsersRepository.create({
+      email: 'will@gmail.com',
+      name: 'will',
+      password: '21312',
+    });
+
+    const transaction = await fakeTransactionsRepository.create({
+      date: new Date(2021, 5, 22),
+      fromBankAccountId: bank.id,
+      note: 'note',
+      symbolCoin: 'BRL',
+      title: 'title',
+      transactionType: 'gain',
+      userId: user.id,
+      value: 123,
+    });
+
+    await fakeTransactionsRepository.create({
+      date: new Date(2021, 6, 21),
+      fromBankAccountId: bank.id,
+      note: 'note',
+      symbolCoin: 'BRL',
+      title: 'title',
+      transactionType: 'gain',
+      userId: user.id,
+      value: 123,
+    });
+
+    const transactions = await listTransactionByDateUserId.execute({
+      date: '2021-06-22',
+      userId: user.id,
+    });
+    expect(transactions).toEqual([transaction]);
+  });
+});
