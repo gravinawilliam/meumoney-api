88fcaf1f1d3758400c6d4d42ec4ebeaa7126b52c
William Gravina <dev.gravinawilliam@gmail.com>
2021-05-30 17:19:18 -0300

    test: must be able to create an expense type transaction

--- src/modules/transactions/services/__test__/CreateTransactionService.spec.ts
+++ src/modules/transactions/services/__test__/CreateTransactionService.spec.ts
@@ -63,6 +63,46 @@ describe('Create Transaction', () => {
     expect(transactionCreated).toHaveProperty('id');
   });
 
+  it('must be able to create an expense type transaction', async () => {
+    const bank = await fakeBanksRepository.create({
+      blueColorCard: 123,
+      greenColorCard: 154,
+      redColorCard: 133,
+      name: 'Nubank',
+    });
+
+    const user = await fakeUsersRepository.create({
+      email: 'will@gmail.com',
+      name: 'will',
+      password: '21312',
+    });
+
+    const bankAccount = await fakeBankAccountsRepository.create({
+      accountNumbers: '123',
+      balance: 1456.5,
+      bankId: bank.id,
+      cardholderName: 'William',
+      monthValidity: 5,
+      yearValidity: 25,
+      symbolCoin: 'BRL',
+      userId: user.id,
+    });
+
+    const transactionCreated = await createTransaction.execute({
+      date: new Date(2021, 6, 21),
+      fromBankAccountId: bankAccount.id,
+      note: 'note',
+      symbolCoin: 'BRL',
+      title: 'title',
+      transactionType: 'expense',
+      userId: user.id,
+      value: 123,
+    });
+
+    expect(transactionCreated).toHaveProperty('id');
+    expect(transactionCreated.transactionType).toBe('expense');
+  });
+
   it('should not be able to create a negative transaction', async () => {
     const bank = await fakeBanksRepository.create({
       blueColorCard: 123,
@@ -177,6 +217,46 @@ describe('Create Transaction', () => {
         userId: user.id,
         value: 123,
         toBankAccountId: 'invalid id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a transaction with an invalid from bank account id', async () => {
+    const bank = await fakeBanksRepository.create({
+      blueColorCard: 123,
+      greenColorCard: 154,
+      redColorCard: 133,
+      name: 'Nubank',
+    });
+
+    const user = await fakeUsersRepository.create({
+      email: 'will@gmail.com',
+      name: 'will',
+      password: '21312',
+    });
+
+    const bankAccount = await fakeBankAccountsRepository.create({
+      accountNumbers: '123',
+      balance: 1456.5,
+      bankId: bank.id,
+      cardholderName: 'William',
+      monthValidity: 5,
+      yearValidity: 25,
+      symbolCoin: 'BRL',
+      userId: user.id,
+    });
+
+    await expect(
+      createTransaction.execute({
+        date: new Date(2021, 6, 21),
+        fromBankAccountId: 'from bank account id invalid',
+        note: 'note',
+        symbolCoin: 'BRL',
+        title: 'title',
+        transactionType: 'transfer',
+        userId: user.id,
+        value: 123,
+        toBankAccountId: bankAccount.id,
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
