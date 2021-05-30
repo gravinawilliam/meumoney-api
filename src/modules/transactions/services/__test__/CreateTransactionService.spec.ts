6434c52bd30afc8f1337038276557ebe124d0a10
William Gravina <dev.gravinawilliam@gmail.com>
2021-05-30 16:43:19 -0300

    test: should not be able to create a negative transaction

--- src/modules/transactions/services/__test__/CreateTransactionService.spec.ts
+++ src/modules/transactions/services/__test__/CreateTransactionService.spec.ts
@@ -2,6 +2,7 @@ import FakeBankAccountsRepository from '@modules/bankAccounts/fakes/FakeBankAcco
 import FakeBanksRepository from '@modules/banks/fakes/FakeBanksRepository';
 import FakeTransactionsRepository from '@modules/transactions/fakes/FakeTransactionsRepository';
 import FakeUsersRepository from '@modules/users/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
 import CreateTransactionService from '../CreateTransactionService';
 
 let fakeTransactionsRepository: FakeTransactionsRepository;
@@ -60,5 +61,44 @@ describe('Create Transaction', () => {
     });
 
     expect(transactionCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a negative transaction', async () => {
+    const bank = await fakeBanksRepository.create({
+      blueColorCard: 123,
+      greenColorCard: 154,
+      redColorCard: 133,
+      name: 'Nubank',
+    });
+
+    const user = await fakeUsersRepository.create({
+      email: 'will@gmail.com',
+      name: 'will',
+      password: '21312',
+    });
+
+    const bankAccount = await fakeBankAccountsRepository.create({
+      accountNumbers: '123',
+      balance: 1456.5,
+      bankId: bank.id,
+      cardholderName: 'William',
+      monthValidity: 5,
+      yearValidity: 25,
+      symbolCoin: 'BRL',
+      userId: user.id,
+    });
+
+    await expect(
+      createTransaction.execute({
+        date: new Date(2021, 6, 21),
+        fromBankAccountId: bankAccount.id,
+        note: 'note',
+        symbolCoin: 'BRL',
+        title: 'title',
+        transactionType: 'gain',
+        userId: user.id,
+        value: -123,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
