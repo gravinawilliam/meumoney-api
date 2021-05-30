383260fd1ef9c07842a6584364d9019971f838cb
William Gravina <dev.gravinawilliam@gmail.com>
2021-05-30 16:14:59 -0300

    feat: method delete created for delete user in interface users repository

--- src/modules/users/interfaces/repositories/IUsersRepository.ts
+++ src/modules/users/interfaces/repositories/IUsersRepository.ts
@@ -3,6 +3,7 @@ import IUser from '../models/IUser';
 
 export default interface IUsersRepository {
   create(user: ICreateUserDTO): Promise<IUser>;
+  delete(user: IUser): Promise<IUser>;
   findByEmail(email: string): Promise<IUser | undefined>;
   findById(id: string): Promise<IUser | undefined>;
 }
