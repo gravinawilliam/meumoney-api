36c4ce8706c55ed6d94424c740e510f9abb62c64
William Gravina <dev.gravinawilliam@gmail.com>
2021-05-31 15:46:43 -0300

    feat: added constant NO_CONTENT in http status code

--- src/shared/constants/HttpStatusCode.ts
+++ src/shared/constants/HttpStatusCode.ts
@@ -1,6 +1,7 @@
 export const OK = 200;
 export const CREATED = 201;
 export const ACCEPTED = 202;
+export const NO_CONTENT = 204;
 export const BAD_REQUEST = 400;
 export const UNAUTHORIZED = 401;
 export const NOT_FOUND = 404;
