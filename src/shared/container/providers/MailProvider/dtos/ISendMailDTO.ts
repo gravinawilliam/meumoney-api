99ddef3a5746d652f085e6cd916e539e6a246446
William Gravina <dev.gravinawilliam@gmail.com>
2021-05-31 15:02:52 -0300

    feat: dto created for send mail

---
+++ src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -0,0 +1,13 @@
+import IParseTemplateDTO from '../../MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+export default interface ISendMailDTO {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseTemplateDTO;
+}
