d66ec87938d9b3404dc6094348e14de1f1100b44
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-02 12:52:38 -0300

    feat: added method findByTransactionIdUserId and delete in fake transactions repository

--- src/modules/transactions/fakes/FakeTransactionsRepository.ts
+++ src/modules/transactions/fakes/FakeTransactionsRepository.ts
@@ -2,6 +2,7 @@ import { getDay, getMonth, getYear } from 'date-fns';
 import { v4 } from 'uuid';
 import Transaction from '../infra/typeorm/entities/Transaction';
 import ICreateTransactionDTO from '../interfaces/dtos/ICreateTransactionDTO';
+import IDeleteTransactionDTO from '../interfaces/dtos/IDeleteTransactionDTO';
 import IListTransactionByDateUserIdDTO from '../interfaces/dtos/IListTransactionByDateUserIdDTO';
 import ITransaction from '../interfaces/models/ITransaction';
 import ITransactionsRepository from '../interfaces/repositories/ITransactionsRepository';
@@ -22,6 +23,13 @@ export default class FakeTransactionsRepository
     return transactionCreated;
   }
 
+  public async delete(transaction: ITransaction): Promise<ITransaction> {
+    const findIndex = this.transactions.findIndex(i => i.id === transaction.id);
+    const deletedTransaction = this.transactions[findIndex];
+    this.transactions.splice(findIndex, 1);
+    return deletedTransaction;
+  }
+
   public async findByDateUserId({
     date,
     userId,
@@ -36,5 +44,16 @@ export default class FakeTransactionsRepository
       );
     });
     return transactions;
+  }
+
+  public async findByTransactionIdUserId({
+    transactionId,
+    userId,
+  }: IDeleteTransactionDTO): Promise<ITransaction | undefined> {
+    const foundBankAccount = this.transactions.find(
+      transaction =>
+        transaction.id === transactionId && transaction.userId === userId,
+    );
+    return foundBankAccount;
   }
 }
