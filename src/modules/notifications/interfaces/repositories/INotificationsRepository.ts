51648e2b7e09dcac2d95d852ebdaa0008d3df481
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-06 10:24:02 -0300

    feat: created method findUnseenNotification in notifications repository

--- src/modules/notifications/interfaces/repositories/INotificationsRepository.ts
+++ src/modules/notifications/interfaces/repositories/INotificationsRepository.ts
@@ -3,5 +3,6 @@ import INotification from '../models/INotification';
 
 export default interface INotificationsRepository {
   create(notification: ICreateNotificationDTO): Promise<INotification>;
+  findUnseenNotification(userId: string): Promise<INotification | undefined>;
   save(notification: INotification): Promise<void>;
 }
