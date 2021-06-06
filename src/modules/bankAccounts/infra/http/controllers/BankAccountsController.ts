8ec1fad3153595d4d37223dd49af50271133fae4
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-06 09:51:03 -0300

    feat: controller created for list bank accounts by user id

--- src/modules/bankAccounts/infra/http/controllers/BankAccountsController.ts
+++ src/modules/bankAccounts/infra/http/controllers/BankAccountsController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
-import { CREATED } from '@shared/constants/HttpStatusCode';
+import { CREATED, OK } from '@shared/constants/HttpStatusCode';
 import CreateBankAccountService from '@modules/bankAccounts/services/CreateBankAccountService';
+import ListBankAccountsByUserIdService from '@modules/bankAccounts/services/ListBankAccountsByUserIdService';
 
 export default class BankAccountsController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -28,5 +29,16 @@ export default class BankAccountsController {
       yearValidity,
     });
     return res.status(CREATED).json(classToClass(httpResponse));
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const userId = req.user.id;
+    const listBankAccountsByUserId = container.resolve(
+      ListBankAccountsByUserIdService,
+    );
+    const httpResponse = await listBankAccountsByUserId.execute({
+      userId,
+    });
+    return res.status(OK).json(classToClass(httpResponse));
   }
 }
