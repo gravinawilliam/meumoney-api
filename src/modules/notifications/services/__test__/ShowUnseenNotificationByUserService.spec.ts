892bfea8a65a633d4323d079ff50963cc4f910cd
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-06 10:42:17 -0300

    must be able to show unseen notification

---
+++ src/modules/notifications/services/__test__/ShowUnseenNotificationByUserService.spec.ts
@@ -0,0 +1,41 @@
+import FakeNotificationsRepository from '@modules/notifications/fakes/FakeNotificationsRepository';
+import FakeUsersRepository from '@modules/users/fakes/FakeUsersRepository';
+import ShowUnseenNotificationByUserService from '../ShowUnseenNotificationByUserService';
+
+let fakeNotificationsRepository: FakeNotificationsRepository;
+let fakeUsersRepository: FakeUsersRepository;
+let showUnseenNotificationByUser: ShowUnseenNotificationByUserService;
+
+describe('Show Unseen Notification By User', () => {
+  beforeEach(() => {
+    fakeNotificationsRepository = new FakeNotificationsRepository();
+    fakeUsersRepository = new FakeUsersRepository();
+    showUnseenNotificationByUser = new ShowUnseenNotificationByUserService(
+      fakeNotificationsRepository,
+      fakeUsersRepository,
+    );
+  });
+
+  it('must be able to show unseen notification', async () => {
+    const user = await fakeUsersRepository.create({
+      email: 'wil@gmail.com',
+      name: 'will',
+      password: '123456789',
+    });
+
+    const notification = await fakeNotificationsRepository.create({
+      content: 'content notification',
+      transactionId: 'transaction id',
+      userId: user.id,
+    });
+
+    await fakeNotificationsRepository.save(notification);
+
+    const foundNotification = await showUnseenNotificationByUser.execute({
+      userId: user.id,
+    });
+
+    expect(foundNotification).toEqual(notification);
+    expect(foundNotification.viewed).toBe(true);
+  });
+});
