c4a6c86aac98c89192b778913556f91f6eaf6a90
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-06 22:17:34 -0300

    feat: created method delete in interface bank accounts repository

--- src/modules/bankAccounts/interfaces/repositories/IBankAccountsRepository.ts
+++ src/modules/bankAccounts/interfaces/repositories/IBankAccountsRepository.ts
@@ -3,7 +3,8 @@ import IBankAccount from '../models/IBankAccount';
 
 export default interface IBankAccountsRepository {
   create(bankAccount: ICreateBankAccountDTO): Promise<IBankAccount>;
-  findByUserId(userId: string): Promise<IBankAccount[]>;
+  delete(bankAccount: IBankAccount): Promise<void>;
   findById(id: string): Promise<IBankAccount | undefined>;
+  findByUserId(userId: string): Promise<IBankAccount[]>;
   save(bankAccount: IBankAccount): Promise<IBankAccount>;
 }
