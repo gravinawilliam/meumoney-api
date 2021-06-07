5fdc5b9df3680f46fadb2f7cf040fcbc6617fe6f
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-06 22:20:14 -0300

    feat: controller created for delete bank account

--- src/modules/bankAccounts/infra/http/controllers/BankAccountsController.ts
+++ src/modules/bankAccounts/infra/http/controllers/BankAccountsController.ts
@@ -4,6 +4,7 @@ import { classToClass } from 'class-transformer';
 import { CREATED, OK } from '@shared/constants/HttpStatusCode';
 import CreateBankAccountService from '@modules/bankAccounts/services/CreateBankAccountService';
 import ListBankAccountsByUserIdService from '@modules/bankAccounts/services/ListBankAccountsByUserIdService';
+import DeleteBankAccountService from '@modules/bankAccounts/services/DeleteBankAccountService';
 
 export default class BankAccountsController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -38,6 +39,17 @@ export default class BankAccountsController {
     );
     const httpResponse = await listBankAccountsByUserId.execute({
       userId,
+    });
+    return res.status(OK).json(classToClass(httpResponse));
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const userId = req.user.id;
+    const { bankAccountId } = req.params;
+    const deleteBankAccount = container.resolve(DeleteBankAccountService);
+    const httpResponse = await deleteBankAccount.execute({
+      userId,
+      bankAccountId,
     });
     return res.status(OK).json(classToClass(httpResponse));
   }
