9be71ce5bf85d7f1c92fe7b25a9acc3fb0de80f4
William Gravina <dev.gravinawilliam@gmail.com>
2021-06-22 22:57:16 -0300

    feat: created method findByBankAccount in interface transactions repository

--- src/modules/transactions/interfaces/repositories/ITransactionsRepository.ts
+++ src/modules/transactions/interfaces/repositories/ITransactionsRepository.ts
@@ -2,11 +2,16 @@ import ICreateTransactionDTO from '../dtos/ICreateTransactionDTO';
 import IDeleteTransactionDTO from '../dtos/IDeleteTransactionDTO';
 import IListTransactionByDateBankAccountIdDTO from '../dtos/IListTransactionByDateBankAccountIdDTO';
 import IListTransactionByDateUserIdDTO from '../dtos/IListTransactionByDateUserIdDTO';
+import IListTransactionsByBankAccountIdDTO from '../dtos/IListTransactionsByBankAccountIdDTO';
 import ITransaction from '../models/ITransaction';
 
 export default interface ITransactionsRepository {
   create(transaction: ICreateTransactionDTO): Promise<ITransaction>;
   delete(transaction: ITransaction): Promise<ITransaction>;
+  findByBankAccount({
+    bankAccountId,
+    userId,
+  }: IListTransactionsByBankAccountIdDTO): Promise<ITransaction[]>;
   findByDateUserId({
     date,
     userId,
